fabe080844d00cd73c68ce45bbb31af602a7010b
Hemraj <thapahemraj141@gmail.com>
2025-09-12 23:04:14 +0530

    Databse Connected

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server running at port : ${port}`);
-});
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(port, () => {
+      console.log(`Server running at port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database Connection Error", err);
+  });
